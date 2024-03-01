1fa0f4be7ccd9bca217ada855f6135efdb94d593
unknown <sakarim9124@gmail.com>
2024-03-02 01:42:55 +0530

    ui updates

--- src/component/CardComponent.js
+++ src/component/CardComponent.js
@@ -15,7 +15,35 @@ const CardComponent = ({ item, handleClick }) => {
     url,
     created,
   } = item;
-  return <li onClick={() => handleClick(name, id)}>{name}</li>;
+  return (
+    <li onClick={() => handleClick(name, id)} className="card_container">
+      {/* left container */}
+      <div className="image_container">
+        <img src={image} alt={name} />
+      </div>
+      {/* right container */}
+      <div className="content_container">
+        <div className="details_section">
+          <strong className="heading_text">
+            <h2>Crystal Dealer</h2>
+          </strong>
+          <span className="status">
+            <span className="status__icon"></span> {status} - {species}
+          </span>
+        </div>
+
+        <div className="details_section">
+          <span className="text-gray">Last known location:</span>
+          <strong className="heading_text">{location?.name}</strong>
+        </div>
+
+        <div className="details_section">
+          <span className="text-gray">First seen in:</span>
+          <strong className="heading_text">{origin?.name}</strong>
+        </div>
+      </div>
+    </li>
+  );
 };
 
 export default CardComponent;
