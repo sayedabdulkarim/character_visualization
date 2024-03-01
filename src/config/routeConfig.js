b75b6c805950535be2211d94a88c06d476b429fd
unknown <sakarim9124@gmail.com>
2024-03-01 23:46:24 +0530

    added route config, initial screens

---
+++ src/config/routeConfig.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import App from "../App";
+//screens
+import HomeScreen from "../screens/auth/Home";
+import CharacterDetailsScreen from "../screens/CharacterDetailsScreen";
+
+const AppRoutes = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<HomeScreen />} />
+          <Route
+            path="characterdetails/:name/:id"
+            element={<CharacterDetailsScreen />}
+          />
+        </Route>
+        <Route path="*" element={<h1>404 Component</h1>} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRoutes;
---
+++ src/screens/CharacterDetailsScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const CharacterDetailsScreen = () => {
+  return (
+    <div>
+      <h1>CharacterDetailsScreen</h1>
+      <h1>CharacterDetailsScreen</h1>
+      <h1>CharacterDetailsScreen</h1>
+      <h1>CharacterDetailsScreen</h1>
+      <h1>CharacterDetailsScreen</h1>
+      <h1>CharacterDetailsScreen</h1>
+    </div>
+  );
+};
+
+export default CharacterDetailsScreen;
