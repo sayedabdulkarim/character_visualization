e4ee77fb2827bc0cfea973a5407c72761e24c58e
unknown <sakarim9124@gmail.com>
2024-03-01 23:37:55 +0530

    setup with rtk, router, scss

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="hello">
+      <h1>App</h1>
+      <h1>App</h1>
+      <h1>App</h1>
+      <h1>App</h1>
+      <h1>App</h1>
+      <h1>App</h1>
+      <h1>App</h1>
+      <h1>App</h1>
+      <h1>App</h1>
+      <h1>App</h1>
+      <h1>App</h1>
+      <h1>App</h1>
+      <h1>App</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/slices/home/homeApiSlice.js
@@ -0,0 +1,21 @@
+import { apiSlice } from "../apiSlice";
+
+const USERS_URL = "api/character";
+
+export const homeApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllCharacters: builder.query({
+      query: (id) => ({
+        url: `${USERS_URL}`,
+      }),
+    }),
+    getCharacterById: builder.query({
+      query: (id) => ({
+        url: `${USERS_URL}/${id}`,
+      }),
+    }),
+  }),
+});
+
+export const { useGetAllCharactersQuery, useGetCharacterByIdQuery } =
+  homeApiSlice;
