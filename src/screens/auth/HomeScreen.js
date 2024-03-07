20c8a11f952cfa00880436a61e9bf7982b850403
unknown <sakarim9124@gmail.com>
2024-03-07 22:03:24 +0530

    updated

--- src/screens/auth/HomeScreen.js
+++ src/screens/auth/HomeScreen.js
@@ -1,76 +1,72 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import {
-  useGetAllCharactersQuery,
-  useGetCharactersByPageQuery,
-} from "../../slices/home/homeApiSlice";
+import { useGetCharactersByPageQuery } from "../../slices/home/homeApiSlice";
 import { setCharactersList } from "../../slices/home/homeSlice";
-import ErrorComponent from "../../component/ErrorComponent";
 import CardComponent from "../../component/CardComponent";
+import PaginationComp from "../../component/pagination";
+import ErrorComponent from "../../component/ErrorComponent";
 
 const HomeScreen = () => {
   //misc
+  const pageSize = 20;
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  // queries n mutation
-  const {
-    data: getAllCharacters,
-    error: getAllCharactersError,
-    isLoading: isLoadingGetAllCharacters,
-  } = useGetAllCharactersQuery();
+  // state
+  const [currentPage, setCurrentPage] = useState(1);
 
-  const {
-    data: getCharactersByPage,
-    error: getCharactersByPageError,
-    isLoading: isLoadinggetCharactersByPage,
-  } = useGetCharactersByPageQuery(2);
+  //queries n mutation
+  const { data, error, isLoading } = useGetCharactersByPageQuery(currentPage);
 
   //func
-  const handleClick = (name, id) => {
-    navigate(`/characterdetails/${name}/${id}`);
+  const totalPage = data ? data?.info.pages : 0;
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
   };
 
   //async
   useEffect(() => {
-    if (getAllCharacters) {
-      dispatch(setCharactersList(getAllCharacters));
+    if (data) {
+      dispatch(setCharactersList(data));
     }
-  }, [getAllCharacters, dispatch]);
+  }, [data, dispatch]);
 
-  console.log(
-    {
-      getCharactersByPage,
-    },
-    " getCharactersByPagegetCharactersByPage"
-  );
-
+  console.log(isLoading, "lllllllllllllllllllllll");
   return (
     <div>
-      <ErrorComponent error={getAllCharactersError} />
+      <ErrorComponent error={error} />
 
-      {isLoadingGetAllCharacters ? (
+      {isLoading ? (
         <div className="loader"></div>
       ) : (
-        <ul className="character_list_container">
-          {getAllCharacters?.results?.map((item) => {
-            const { id } = item;
-            return (
-              <li
-                key={id}
-                // onClick={() => handleClick(name, id)}
-                className="card_container"
-              >
-                <CardComponent
-                  item={item}
-                  handleClick={handleClick}
-                  isView={true}
-                />
-              </li>
-            );
-          })}
-        </ul>
+        <>
+          <ul className="character_list_container">
+            {data?.results?.map((item) => {
+              const { id } = item;
+              return (
+                <li key={id} className="card_container">
+                  <CardComponent
+                    item={item}
+                    handleClick={() =>
+                      navigate(`/characterdetails/${item.name}/${id}`)
+                    }
+                    isView={true}
+                  />
+                </li>
+              );
+            })}
+          </ul>
+          <PaginationComp
+            className="pagination-bar w-25 mb-0 px-3"
+            currentPage={currentPage}
+            totalCount={data ? data.info.count : 0}
+            pageSize={pageSize}
+            onPageChange={handlePageChange}
+            selectedPage={currentPage}
+            totalPage={totalPage}
+          />
+        </>
       )}
     </div>
   );
